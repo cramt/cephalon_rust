16f9666b7ebda0a3dea93dbdbb676fbfb024566b
Alexandra Østermark <alex.cramt@gmail.com>
2024-08-31 11:44:29 +0200

    parse logs lets go

--- daemon/src/log_watcher/mod.rs
+++ daemon/src/log_watcher/mod.rs
@@ -1,6 +1,7 @@
-use std::{ffi::OsStr, io::SeekFrom, path::PathBuf, time::Duration};
+use std::{ffi::OsStr, io::SeekFrom, path::PathBuf, str::FromStr, time::Duration};
 
 use async_watcher::{notify::RecursiveMode, AsyncDebouncer};
+use ctreg::regex;
 use tokio::{
     fs::File,
     io::{AsyncBufReadExt, AsyncSeekExt, BufReader},
@@ -54,7 +55,58 @@ async fn raw_watcher() -> Result<
     Ok(rx)
 }
 
-pub async fn watcher() {
+regex! { LogEntryParser = r#"(?:\d+\.\d+ )?(?<system>[A-z]+) \[(?<level>[A-z]+)\]: (?<rest>.*)"# }
+regex! { LogScriptEntryParser = r#"(?<script>[A-z]+\.lua: (?<rest>.*))"# }
+
+#[derive(Debug)]
+pub enum LogEntry {
+    SysInfo(String),
+    SysWarning(String),
+    SysError(String),
+    NetInfo(String),
+    PhysInfo(String),
+    SndInfo(String),
+    GfxInfo(String),
+    InputInfo(String),
+    AIInfo(String),
+    GameInfo(String),
+    ScriptInfo { script: String, content: String },
+}
+
+impl FromStr for LogEntry {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.trim();
+        let captures = LogEntryParser::new().captures(s).ok_or(())?;
+        let system = captures.system.content.trim();
+        let level = captures.level.content.trim();
+        let rest = captures.rest.content.trim();
+        match (system, level) {
+            ("Sys", "Info") => Ok(Self::SysInfo(rest.to_string())),
+            ("Sys", "Warning") => Ok(Self::SysWarning(rest.to_string())),
+            ("Sys", "Error") => Ok(Self::SysError(rest.to_string())),
+            ("Net", "Info") => Ok(Self::NetInfo(rest.to_string())),
+            ("Phys", "Info") => Ok(Self::PhysInfo(rest.to_string())),
+            ("Snd", "Info") => Ok(Self::SndInfo(rest.to_string())),
+            ("Gfx", "Info") => Ok(Self::GfxInfo(rest.to_string())),
+            ("Input", "Info") => Ok(Self::InputInfo(rest.to_string())),
+            ("AI", "Info") => Ok(Self::AIInfo(rest.to_string())),
+            ("Game", "Info") => Ok(Self::GameInfo(rest.to_string())),
+            ("Script", "Info") => {
+                let captures = LogScriptEntryParser::new().captures(rest).ok_or(())?;
+                Ok(Self::ScriptInfo {
+                    script: captures.script.content.trim().to_string(),
+                    content: captures.rest.content.trim().to_string(),
+                })
+            }
+            _ => Err(()),
+        }
+    }
+}
+
+pub async fn watcher() -> tokio::sync::mpsc::Receiver<LogEntry> {
+    let (tx, rx) = tokio::sync::mpsc::channel(100);
     let mut file = BufReader::new(File::open(get_default_path()).await.unwrap());
     file.seek(SeekFrom::End(0)).await.unwrap();
     let mut buffer = Vec::with_capacity(50);
@@ -67,9 +119,14 @@ pub async fn watcher() {
                 }
                 let str = String::from_utf8(buffer).unwrap();
                 buffer = Vec::with_capacity(50);
-                println!("{:?}", str.trim());
+                if let Ok(entry) = str.parse() {
+                    tx.send(entry).await.unwrap();
+                } else {
+                    println!("failed to parse log entry: {str}");
+                }
             }
             sleep(Duration::from_millis(100)).await;
         }
     });
+    rx
 }
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,15 +1,4 @@
-use std::{
-    collections::{HashMap, HashSet},
-    ffi::OsStr,
-    fmt::format,
-    process::Stdio,
-};
-
-use config::settings;
-use items::db_reset;
 use log_watcher::watcher;
-use sysinfo::{Pid, System};
-use tokio::process::Command;
 
 pub mod config;
 pub mod items;
@@ -17,48 +6,9 @@ pub mod log_watcher;
 
 #[tokio::main]
 async fn main() {
-    let sys = System::new_all();
-    let warframe = sys
-        .processes_by_exact_name(OsStr::new("Warframe.x64.ex"))
-        .next()
-        .unwrap();
-    let envs = warframe
-        .environ()
-        .iter()
-        .flat_map(|x| {
-            let a = x.as_encoded_bytes();
-            let mut split = a.splitn(2, |b| *b == b'=');
-            let split1 = split.next()?;
-            let split2 = split.next()?;
-            let split1 = unsafe { OsStr::from_encoded_bytes_unchecked(split1) };
-            let split2 = unsafe { OsStr::from_encoded_bytes_unchecked(split2) };
-            Some((split1, split2))
-        })
-        .collect::<HashMap<_, _>>();
-    let wine_envs = envs
-        .into_iter()
-        .filter(|(k, _)| {
-            k.to_str()
-                .map(|x| x.starts_with("WINE") && x != "WINESERVERSOCKET")
-                .unwrap_or(false)
-        })
-        .collect::<HashMap<_, _>>();
-    let wine_exec = wine_envs
-        .get(OsStr::new("WINELOADER"))
-        .unwrap()
-        .to_str()
-        .unwrap();
-    println!("{:?}", wine_envs);
-    let result = Command::new(wine_exec)
-        .arg(&settings().await.overlay_path)
-        .stdin(Stdio::inherit())
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .envs(wine_envs)
-        .spawn()
-        .unwrap()
-        .wait()
-        .await
-        .unwrap();
-    println!("{:?}", result);
+    let mut rx = watcher().await;
+
+    while let Some(i) = rx.recv().await {
+        println!("{:?}", i);
+    }
 }
